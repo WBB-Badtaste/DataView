1dd935366a41274c1e97767eb5d8661039deaccc
Binbin Wu <Binbin Wu>
2017-02-28 21:45:05 +0800

    update

--- app/main.js
+++ app/main.js
@@ -1,22 +1,28 @@
-// ./app/main.js
-
 //采用javascript严格模式
-'use strict';
+//'use strict';
 
 // 应用的控制模块
-const electron = require('electron');
-const app = electron.app;
+const electron = require('electron')
+const app = electron.app
 
 const VERSION = "v1.0.0"
 const APPNAME = "DataView"
 app.setName(APPNAME)
 
-app.on('window-all-closed', () => {app.quit();} );
+let mainWindow = null
 
-app.on('ready', () => {
-  var mainWindow = new electron.BrowserWindow();
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-  mainWindow.maximize();
-  mainWindow.on('closed', () => {mainWindow = null;});
-  //mainWindow.openDevTools();
-});
+function CreateWindow() {
+  mainWindow = new electron.BrowserWindow({show:false})
+  mainWindow.loadURL('file://' + __dirname + '/index.html')
+  mainWindow.maximize()
+  mainWindow.on('closed', ()=>{mainWindow = null})
+  //mainWindow.once('ready-to-show', ()=>{mainWindow.show()})
+  //mainWindow.openDevTools()
+}
+
+app.on('window-all-closed', ()=>{app.quit()})
+
+app.on('ready', CreateWindow);
+
+app.on('activate', ()=>{if(mainWindow == null){CreateWindow()}})
+
