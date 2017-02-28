b6058fe6597c6c53f5ec5d847e9e8d6e250e5a58
吴彬彬 <jomar@wubinbins-MacBook-Air.local>
2017-02-28 20:05:52 +0800

    update

--- app/main.js
+++ app/main.js
@@ -6,38 +6,18 @@
 // 应用的控制模块
 const electron = require('electron');
 const app = electron.app;
-
-// 创建原生浏览器窗口的模块
 const BrowserWindow = electron.BrowserWindow;
-var mainWindow = null;
 
-// 当所有窗口都关闭的时候退出应用
-app.on('window-all-closed', function () {
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
-});
+const VERSION = "v1.0.0"
+const APPNAME = "DataView"
+app.setName(APPNAME)
 
-// 当 Electron 结束的时候，这个方法将会生效
-// 初始化并准备创建浏览器窗口
-app.on('ready', function () {
+app.on('window-all-closed', () => {app.quit();} );
 
-  // 创建浏览器窗口.
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
-
-  // 载入应用的 index.html
+app.on('ready', () => {
+  var mainWindow = new BrowserWindow();
   mainWindow.loadURL('file://' + __dirname + '/index.html');
-
-  // 打开开发工具
-  mainWindow.openDevTools();
-
-  // 窗口关闭时触发
-  mainWindow.on('closed', function () {
-
-    // 想要取消窗口对象的引用，如果你的应用支持多窗口，
-    // 通常你需要将所有的窗口对象存储到一个数组中，
-    // 在这个时候你应该删除相应的元素
-    mainWindow = null;
-  });
-
+  mainWindow.maximize();
+  mainWindow.on('closed', () => {mainWindow = null;});
+  //mainWindow.openDevTools();
 });
