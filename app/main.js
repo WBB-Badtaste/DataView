a2474b380fe6e6d7aaee433667d8fe54e351bad8
吴彬彬 <jomar@wubinbins-MacBook-Air.local>
2017-03-01 12:10:38 +0800

    update

--- app/main.js
+++ app/main.js
@@ -2,27 +2,45 @@
 //'use strict';
 
 // 应用的控制模块
-const electron = require('electron')
-const app = electron.app
+const {app, BrowserWindow, dialog}= require('electron')
 
-const VERSION = "v1.0.0"
-const APPNAME = "DataView"
-app.setName(APPNAME)
-
-let mainWindow = null
+var mainWindow = null
 
 function CreateWindow() {
-  mainWindow = new electron.BrowserWindow({show:false})
-  mainWindow.loadURL('file://' + __dirname + '/index.html')
+  mainWindow = new BrowserWindow({show:false})
+  mainWindow.loadURL('file://' + __dirname + '/Index.html')
   mainWindow.maximize()
-  mainWindow.on('closed', ()=>{mainWindow = null})
-  //mainWindow.once('ready-to-show', ()=>{mainWindow.show()})
-  //mainWindow.openDevTools()
+  mainWindow.on('closed',           ()=>{mainWindow = null})
+  mainWindow.once('ready-to-show',  ()=>{mainWindow.show()})
 }
 
-app.on('window-all-closed', ()=>{app.quit()})
+app.on('window-all-closed', ()=> {
+  /*
+  var dirList = dialog.showOpenDialog({
+    browserWindow: this,
+    title: "Open Config File",
+    defaultPath: "./Config",
+    filters: [
+      {name: 'Images', extensions: ['jpg', 'png', 'gif']},
+      {name: 'Movies', extensions: ['mkv', 'avi', 'mp4']},
+      {name: 'Custom File Type', extensions: ['as']},
+      {name: 'All Files', extensions: ['*']}
+    ],  
+    properties: ["openFile","createDirectory","multiSelections"]
+  })
+  dirList.forEach(function(dir) {
+    console.log(dir)
+  }, this);*/
+  app.quit()
+})
 
-app.on('ready', CreateWindow);
+app.on('ready', ()=> {
+  CreateWindow()
+})
 
-app.on('activate', ()=>{if(mainWindow == null){CreateWindow()}})
+app.on('activate', ()=> {
+  if(mainWindow == null) {
+    CreateWindow()
+  }
+})
 
